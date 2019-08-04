669444e38437d3d6c086e78e47e57e426d9238fa
PsyNut <48391846+psynut@users.noreply.github.com>
2019-08-03 23:19:01 -0700

    Setting Up
    Have the class and two constructors defined. How to save list, though?

---
+++ Assets/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class MyClass{
+
+	public int identity;
+	public string theName;
+	public double doubleNumber;
+
+	public MyClass(int id, string name){
+		identity = id;
+		theName = name; 
+	}
+
+	public MyClass(int id, string name, double num){
+		identity = id;
+		theName = name;
+		doubleNumber = num;
+	}
+}
