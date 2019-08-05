c648eb0874153ed1c1e974180a6ceaf8d7abf6da
PsyNut <48391846+psynut@users.noreply.github.com>
2019-08-05 00:28:30 -0700

    Set up two classes MyClass and MyListClass
    Still need to get MyClass into lists for MyListClass...

---
+++ Assets/JSON Recorder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONRecorder : MonoBehaviour {
+
+	private string path;
+	private string filename = "MyList.JSON";
+
+	public int id = 22;
+	public string nameString = "BubbaGump";
+	public double doubleNum = 4.00;
+
+
+	//Will still need to initialize, but it makes sense to initialize in start.
+	MyListClass myListClass;
+//	MyListClass myListClass = new MyListClass();
+	MyClass myClass;
+//	MyClass myClass = new MyClass();
+
+	// Use this for initialization
+	void Start () {
+		filename = "MyList.JSON";
+		path = Application.persistentDataPath + "/" +filename;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/MyClass.cs
+++ Assets/MyClass.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-
 public class MyClass{
 
 	public int identity;
