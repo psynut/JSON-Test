a3423a0ddcb5eedafb25367410dbd5bee9a704eb
PsyNut <48391846+psynut@users.noreply.github.com>
2019-08-07 00:12:47 -0700

    fixing up the merger mess

--- Assets/MyClass.cs
+++ Assets/MyClass.cs
@@ -10,8 +10,6 @@ public class MyClass{
 	public string theName;
 	public double doubleNumber;
 
-        [System.Serializable]
-
 	public MyClass(int id, string name){
 		identity = id;
 		theName = name; 
