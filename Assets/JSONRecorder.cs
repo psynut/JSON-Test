77c70fb6cdf5ee4a3a5f011fd9083a49a9945080
PsyNut <48391846+psynut@users.noreply.github.com>
2019-08-17 15:57:19 -0700

    Adding Comments
    Still need ot figure out what to do when there is only one MyClass in the MyClassList

--- Assets/JSONRecorder.cs
+++ Assets/JSONRecorder.cs
@@ -6,20 +6,24 @@ using UnityEngine.UI;
 
 public class JSONRecorder : MonoBehaviour {
 
+	//declare variables for JSON file
 	private string path;
 	private string filename = "MyList.JSON";
 
+	//declare fields to be moved to MyClass
 	public int integer;
 	public string nameString;
 	public double doubleNum;
 
+	//InputFields in Scene
 	public InputField integerInput, nameInput, doubleInput;
 
+	//declare myClass and also the list class to be saved in JSON 
 	public MyClass myClass;
 	public MyClassList myClassList =  new MyClassList();
 
 	void Start () {
-		path = Application.persistentDataPath + "/" +filename;
+		path = Application.persistentDataPath + "/" +filename; //path where JSON file goes
 	}
 
 	public void ButtonPush(){
@@ -27,12 +31,15 @@ public class JSONRecorder : MonoBehaviour {
 		RecordIntegerName(integer, nameString, doubleNum);
 	}
 
+	//Takes values in the inputfields
 	void GetInputValues(){
 		integer = int.Parse (integerInput.text);
 		nameString = nameInput.text;
 		doubleNum = double.Parse(doubleInput.text);
 	}
 
+	//If there is a MyClassList already in JSON - move it into myClassList
+	//before adding new entries to the MyClassList
 	void PullData(){
 		if(File.Exists(path)){
 			string contents = File.ReadAllText(path);
@@ -40,6 +47,7 @@ public class JSONRecorder : MonoBehaviour {
 		}
 	}
 
+	//Take add the inputs to a MyClass and add MyClass to the MyClassList
 	void RecordIntegerName(int i, string s, double d) {
 		PullData();
 		myClass = new MyClass(i, s, d);
--- Assets/JSONRetriever.cs
+++ Assets/JSONRetriever.cs
@@ -6,18 +6,22 @@ using System.IO;
 
 public class JSONRetriever : MonoBehaviour {
 
+	//declare variables for JSON file
 	private string path;
 	private string filename = "MyList.JSON";
 
+	//declare text fields in scene
 	public Text integerText, nameText, doubleText;
 
+	//dropdown object in scene
 	public Dropdown dropdown;
 
+	//declare the myClassList
 	public MyClassList myClassList;
 
 	// Use this for initialization
 	void Start () {
-		path = Application.persistentDataPath + "/" +filename;
+		path = Application.persistentDataPath + "/" +filename; //path where JSON file is
 		CheckForFile ();
 		SetupDropdown ();
 	}
@@ -27,10 +31,9 @@ public class JSONRetriever : MonoBehaviour {
 		
 	}
 
+	//if the JSON file is not there, make an empty myClassList and an empty JSON file
 	private void CheckForFile (){
-		if (File.Exists (path)) {
-			readJSON();
-		} else {
+		if (!File.Exists (path)) {
 			List<MyClass> freshList = new List<MyClass>();
 			myClassList.classList = freshList;
 			string contents = JsonUtility.ToJson(myClassList);
@@ -38,6 +41,9 @@ public class JSONRetriever : MonoBehaviour {
 		}
 	}
 
+	//Once JSON is in myClassList, count the instances of MyClasses in the myClassList
+	//make a selection avaialable in the dropdown for each instance.
+	//Have to clear the dropdown each time or it will add duplicates
 	public void SetupDropdown(){
 		readJSON();
 		dropdown.ClearOptions();
@@ -48,6 +54,8 @@ public class JSONRetriever : MonoBehaviour {
 		dropdown.AddOptions(options);
 	}
 
+	//TODO figure out how to make this work if there is only one instance of MyClass
+	// When the selection is chosen in the dropdown menu, display the fields in the text fields aboe
 	public void DropdownSelected(int j){
 		Debug.Log("DropdownSelected(" + j + ")");
 		MyClass myClass = RetrieveMyClass(j);
@@ -56,10 +64,12 @@ public class JSONRetriever : MonoBehaviour {
 		doubleText.text = myClass.doubleNumber.ToString();
 	}
 
+	//Get the MyClass instance in 
 	private MyClass RetrieveMyClass(int j){
-		return myClassList.classList[j];
+		return myClassList.classList[j]; //The MyClass is actually stored in the list classList in myClassList class
 	}
 
+	//Take the JSON file and move it into the myClassList
 	private void readJSON(){
 		string contents = File.ReadAllText (path);
 		myClassList = JsonUtility.FromJson<MyClassList> (contents);
